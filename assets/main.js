990e0e0a593bdd214dec9a11abf4cf8fc69ed79f
Lee Lawler <lawler_lee@icloud.com>
2020-02-04 21:14:13 -0600

    added moment to determine mins away

--- assets/main.js
+++ assets/main.js
@@ -1,41 +1,57 @@
-$(document).ready(function () {
-    // Your web app's Firebase configuration
-    var firebaseConfig = {
-        apiKey: "AIzaSyDjpZey_MDPUMu3YnaRXU_9_bfZVfbiVAc",
-        authDomain: "train-scheduler-857b5.firebaseapp.com",
-        databaseURL: "https://train-scheduler-857b5.firebaseio.com",
-        projectId: "train-scheduler-857b5",
-        storageBucket: "train-scheduler-857b5.appspot.com",
-        messagingSenderId: "346017035702",
-        appId: "1:346017035702:web:89863ba8ba3c862856fd17"
-    };
-    // Initialize Firebase
-    firebase.initializeApp(firebaseConfig);
+// Your web app's Firebase configuration
+var firebaseConfig = {
+    apiKey: "AIzaSyDjpZey_MDPUMu3YnaRXU_9_bfZVfbiVAc",
+    authDomain: "train-scheduler-857b5.firebaseapp.com",
+    databaseURL: "https://train-scheduler-857b5.firebaseio.com",
+    projectId: "train-scheduler-857b5",
+    storageBucket: "train-scheduler-857b5.appspot.com",
+    messagingSenderId: "346017035702",
+    appId: "1:346017035702:web:89863ba8ba3c862856fd17"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
 
-    var database = firebase.database();
+var database = firebase.database();
 
-    $("#addTrain").on("click", function () {
-        event.preventDefault()
-        var name = $("#trainName").val().trim();
-        var destination = $("#trainDest").val().trim();
-        var time = $("#trainTime").val().trim();
-        var frequency = $("#trainFreq").val().trim();
+function clearTable() {
+    $("#trainName").val(" ")
+    $("#trainDest").val(" ")
+    $("#trainTime").val(" ")
+    $("#trainFreq").val(" ")
+};
 
-        database.ref().push({
-            name: name,
-            destination: destination,
-            time: time,
-            frequency: frequency
-        });
+$("#addTrain").on("click", function () {
+    event.preventDefault()
+    var name = $("#trainName").val().trim();
+    var destination = $("#trainDest").val().trim();
+    var time = $("#trainTime").val().trim();
+    var frequency = $("#trainFreq").val().trim();
+
+    database.ref().push({
+        name: name,
+        destination: destination,
+        time: time,
+        frequency: frequency
     });
 
-    database.ref().on("child_added", function(snapshot) {
-        $("tbody").append("<tr>" +
-        snapshot.val().name +
-        "<td>" + snapshot.val().destination +
-        "<td>" + snapshot.val().time +
-        "<td>" + snapshot.val().frequency
-        );
-    });
+    clearTable();
+});
 
+database.ref().on("child_added", function (childSnapshot) {
+
+    var firstTime = childSnapshot.val().time
+    var firstTimeConverted = moment(firstTime, "HH:mm").subtract(1, "years");
+    var timeDiff = moment().diff(moment(firstTimeConverted), "minutes");
+    var arrival = childSnapshot.val().frequency;
+    var remainder = timeDiff % arrival;
+    var minsTil = arrival - remainder;
+    var next = moment().add(minsTil, "minutes");
+
+    $("tbody").append("<tr><td>" + childSnapshot.val().name +
+        "</td><td>" + childSnapshot.val().destination +
+        "</td><td>" + childSnapshot.val().frequency +
+        "</td><td>" + next.format("HH:mm") +
+        "</td><td>" + minsTil +
+        "</td></tr>"
+    );
 });
